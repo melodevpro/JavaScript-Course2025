55f0dbeb201c76d7697c8c3146dcc1e5b0620716
melomatthewdev <devscustomers@outlook.com>
2025-02-10 08:09:05 -0500

    class 2

--- main.js
+++ main.js
@@ -23,4 +23,77 @@ console.log(name1 + " " + name2, `${name1} ${name2}`);
 let isTrue = true;
 console.log(isTrue ? "True" : "False");
 
-let 
+const  showRecipeOne = false;
+
+function getRecipeOneName(recipeName){
+    return recipeName;
+}
+
+function getRecipeTwoName(recipeName){
+    return recipeName;
+}
+
+if (showRecipeOne){
+    console.log(getRecipeOneName("Pizza"));
+} else {
+    console.log(getRecipeTwoName("Coke"));
+}
+
+showRecipeOne
+? console.log(getRecipeOneName("Pizza")) 
+: console.log(getRecipeTwoName("Coke"));
+
+// 
+
+const product = {
+    name: "Melo",
+    price: 100,
+    isAvailable: true,
+    rating: 8,
+};
+console.log(product);
+
+const product2 = {
+    description: "Product 2 description",
+    
+};
+
+
+const { description } = product2;
+console.log(description);
+
+const array = [1, 2, 3, 4, 5];
+
+let getArrayFirstValue = array[0];
+let getArraySecondValue = array[1];
+console.log(getArrayFirstValue, getArraySecondValue);
+
+const [
+    arrayFirstElment,
+    arraySecondElement,
+    arrayThirdElement,
+    arrayFourthElement,
+] = array;
+
+console.log(
+    arrayFirstElment,
+    arraySecondElement,
+    arrayThirdElement,
+    arrayFourthElement,
+);
+
+// default parameters, spread and rest operators
+function mulOfTwoNumbers(num1 = 1, num2 = 2) {
+    console.log(num1, num2);
+    return num1 * num2;
+}
+console.log(mulOfTwoNumbers(10));
+
+const array2 = [2, 3, 4];
+const array3 = [10, 11, 12];
+console.log([999, ...array2, 90, ...array3, 10000]);
+
+
+
+
+
