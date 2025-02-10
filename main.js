2fe8a8b08fc99d6c62e4c8d896e376e5f46cbf10
melomatthewdev <devscustomers@outlook.com>
2025-02-10 07:14:28 -0500

    ES6 concepts

---
+++ main.js
@@ -0,0 +1,26 @@
+// Logical AND (&&) and Logical OR(||)
+let a = true;
+let b = true;
+console.log(a && b);
+console.log(a || b);
+
+let c = false;
+let d = true;
+console.log(c && d);
+console.log(c || d);
+
+let e = false;
+let f = false;
+console.log(e && f);
+console.log(e || f);
+
+// Template literals
+let name1 = "Melo";
+let name2 = "Matthew";
+console.log(name1 + " " + name2, `${name1} ${name2}`);
+
+// Ternary operator
+let isTrue = true;
+console.log(isTrue ? "True" : "False");
+
+let 
