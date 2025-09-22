570ebc0cb741a73b486cc3edefed6b97cd9a6425
melomatthewdev <devscustomers@outlook.com>
2025-09-22 10:50:07 -0500

    tipos de datos en javascript

---
+++ datos/tipos-datos.js
@@ -0,0 +1,28 @@
+// tipos primitivos
+
+//numeros:
+let valorNumeral = 1000;
+valorNUmeral = 1001;
+
+// strings:
+const valorString = "hola javascript";
+
+//booleanos:
+const valorBooleano = true;
+
+//symbol:
+const valorSymbol = Symbol();
+
+//Indefinidos:
+const valorIndefinido = undefined;
+
+//Nulos:
+const valorNulo = mull;
+
+//Tipos no primitivos:
+
+//Objetos:
+const objeto = { nombre: "Melo", apellido: "Duarte" };
+
+//Arrays o arreglos:
+const array = [1, 2, 3, 4];
---
+++ datos/tipos-null.js
@@ -0,0 +1,17 @@
+let miVariable = null;
+console.log(miVariable);
+
+function retornoNull(){
+    return null;
+}
+
+let resultado = retornoNull();
+console.log(resultado);
+
+let miVariable2;
+console.loh(miVariable2);
+
+function muestraEnConsola(parametro){
+    console.log(parametro);
+}
+muestraEnConsola();
